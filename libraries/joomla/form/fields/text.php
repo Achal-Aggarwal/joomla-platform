b4f7e74dc829c6177000ea1e8677c5ec3d55c922
Achal-Aggarwal <theachalaggarwal@gmail.com>
2013-05-05 02:27:30 +0530

    Changing class attribute in JFormFieldText.

--- libraries/joomla/form/fields/text.php
+++ libraries/joomla/form/fields/text.php
@@ -41,7 +41,7 @@ class JFormFieldText extends JFormField
 		// Initialize some field attributes.
 		$size = $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
 		$maxLength = $this->element['maxlength'] ? ' maxlength="' . (int) $this->element['maxlength'] . '"' : '';
-		$class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
+		$class = !empty($this->class) ? ' class = "' . explode(" ", $this->class) . "'" : "";
 		$readonly = ((string) $this->element['readonly'] == 'true') ? ' readonly="readonly"' : '';
 		$disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
 		$required = $this->required ? ' required="required" aria-required="true"' : '';
